9a5b46c036165d0298dac3e8a03fe055d9de6b5e
Igor Postnikov <igor.postnikov@8x8.com>
2019-12-26 20:02:35 -0800

    create repo

---
+++ src/main/java/com/newtours/pages/RegistrationPage.java
@@ -0,0 +1,21 @@
+package com.newtours.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class RegistrationPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public RegistrationPage(WebDriver driver){
+
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
+    }
+
+    public void goTo(){
+        this.driver.get("http://newtours.demoaut.com/mercuryregister.php");
+    }
+
+}
