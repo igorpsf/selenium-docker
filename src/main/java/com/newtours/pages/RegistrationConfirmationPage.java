f5783e3db3d596b458fd35e0014b25674f32b358
Igor Postnikov <igor.postnikov@8x8.com>
2019-12-27 12:10:46 -0800

    Added Findflight and flightdetails pages

--- src/main/java/com/newtours/pages/RegistrationConfirmationPage.java
+++ src/main/java/com/newtours/pages/RegistrationConfirmationPage.java
@@ -4,14 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegistrationConfirmationPage {
 
-    public WebDriver driver;
-    public WebDriverWait wait;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @FindBy(partialLinkText = "sign-in")
     private WebElement signinLink;
